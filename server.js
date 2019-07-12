651b027bcdd4805fb0bacda01cfa38642fb07ab8
lipeng <lipeng@daokoudai.com>
2019-07-12 15:28:49 +0800

    4.Node接口搭建-连接MongoDB数据库

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
+const db = require('./config/keys').mongoURI
+console.log(db)
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+    )
+  .then(()=>{console.log('MongoDB Connected')})
+  .catch(err=>console.log(err))
 app.get(
   '/',
   (req,res)=>{
