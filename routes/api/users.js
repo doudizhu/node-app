7c98e9f1380876a96eb00917ab8b971cd8ec3209
lipeng <lipeng@daokoudai.com>
2019-07-12 19:02:46 +0800

    7.Node接口搭建-搭建注册接口并存储数据(0.接口参数请求&保存数据库；1.bodyParser：post请求解析；2.bcrypt：密码加密)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 // @login & register
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+
+const User = require('../../models/User')
 
 // $router GET api/users/test
 // @desc 返回请求的json数据
@@ -13,6 +16,46 @@ router.get(
         msg: 'login works',
       }
     )
+  }
+)
+
+// $router POST api/users/register
+// @desc 返回请求的json数据
+// @access public
+router.post(
+  '/register',
+  (req,res)=>{
+    // console.log(req.body)
+    
+    // 查询数据中是否拥有邮箱
+    User.findOne({email: req.body.email})
+      .then((user)=>{
+        if(user){
+          return res.status(400).json({email:'邮箱已被注册!'})
+        }else{
+
+          const newUser = new User({
+            name:req.body.name,
+            email:req.body.email,
+            avatar,
+            password:req.body.password
+          })
+
+          bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(newUser.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) throw err;
+
+                newUser.password = hash
+                console.log()
+
+                newUser.save()
+                  .then(user=>res.json(user))
+                  .catch(err=>console.log(err))
+            });
+          });
+        }
+      })
   }
 )
 
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // 引入users.js
 const users = require('./routes/api/users')
 
 // DB config
 const db = require('./config/keys').mongoURI
+
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
 // Connect to mongodb
 console.log(db)
 mongoose
