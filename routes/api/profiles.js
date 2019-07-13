567e0fe9e0b6c17be4a69afef3925f763214fbe7
lipeng <lipeng@daokoudai.com>
2019-07-13 21:01:37 +0800

    17.Node接口搭建-获取所有以及单个信息v0.2 获取单个信息

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -46,5 +46,18 @@ router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
     })
     .catch(err=>res.status(404).json(err))
 })
+// $router GET api/profiles/:id
+// @desc 获取单个信息
+// @access Private
+router.get('/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  Profile.findOne({_id:req.params.id})
+    .then(profile=>{
+      if(!profile){
+        return res.status(404).json('没有任何内容')
+      }
+      res.json(profile)
+    })
+    .catch(err=>res.status(404).json(err))
+})
 
 module.exports = router
