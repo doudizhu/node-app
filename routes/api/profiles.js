692b7ec88627c945358776594a1b1cf4f6cf1f03
lipeng <lipeng@daokoudai.com>
2019-07-13 20:46:21 +0800

    16.Node接口搭建-添加信息接口

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -14,4 +14,23 @@ router.get('/test',(req,res)=>{
   })
 })
 
+
+// $router POST api/profiles/add
+// @desc 创建信息接口
+// @access Private
+router.post('/add',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  const profileFields = {}
+
+  if(req.body.type) profileFields.type = req.body.type
+  if(req.body.describe) profileFields.describe = req.body.describe
+  if(req.body.income) profileFields.income = req.body.income
+  if(req.body.expend) profileFields.expend = req.body.expend
+  if(req.body.cash) profileFields.cash = req.body.cash
+  if(req.body.remark) profileFields.remark = req.body.remark
+
+  new Profile(profileFields).save().then(profile=>{
+    res.json(profile)
+  })
+})
+
 module.exports = router
