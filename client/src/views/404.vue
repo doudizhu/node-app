0a341ef06fcf0dac30b572586639ee92d3c29744
lipeng <lipeng@daokoudai.com>
2019-07-15 11:43:03 +0800

    21.VueCli3.0全栈-设置Register和404组件

---
+++ client/src/views/404.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .notfound
+    img(src='../assets/404.gif')
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang='stylus' scoped>
+.notfound
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  img
+    width: 100%;
+    height: 100%
+</style>
